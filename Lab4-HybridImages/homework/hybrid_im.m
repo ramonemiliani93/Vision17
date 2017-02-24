63a18dc9247b2105b44ed7379d488f9fa83523b6
Alejandro Posada <a.posada10@uniandes.edu.co>
2017-02-23 19:18:42 -0500

    Add some order to the folders

--- Lab4-HybridImages/homework/hybrid_im.m
+++ Lab4-HybridImages/homework/hybrid_im.m
@@ -4,10 +4,10 @@ clc; clear all; close all;
 sigma = 8; % Param. of the Gaussian radius
 
 %Load and resize images
-I1 = imread('alejandro_orig.png');
-I2 = imread('ramon_orig.png');
+I1 = imread('Images/alejandro_orig.png');
+I2 = imread('Images/ramon_orig.png');
 I1_crop = imresize(I1, [size(I2,1), size(I2,2)]);
-imwrite(I1_crop, 'alejandro_crop.png')
+imwrite(I1_crop, 'Images/alejandro_crop.png')
 
 %Compute hybrid image
 hybrid_im = I2-imgaussfilt(I2, sigma) + imgaussfilt(I1_crop, sigma);
