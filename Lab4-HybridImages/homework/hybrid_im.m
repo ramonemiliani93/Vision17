5a4463c75f9cf3b47cfc2bbd68deff975e4fb57c
Alejandro Posada <a.posada10@uniandes.edu.co>
2017-02-23 19:03:25 -0500

    Add homework

---
+++ Lab4-HybridImages/homework/hybrid_im.m
@@ -0,0 +1,14 @@
+%Obtain a hybrid image from two different images
+
+clc; clear all; close all;
+sigma = 8; % Param. of the Gaussian radius
+
+%Load and resize images
+I1 = imread('alejandro_orig.png');
+I2 = imread('ramon_orig.png');
+I1_crop = imresize(I1, [size(I2,1), size(I2,2)]);
+imwrite(I1_crop, 'alejandro_crop.png')
+
+%Compute hybrid image
+hybrid_im = I2-imgaussfilt(I2, sigma) + imgaussfilt(I1_crop, sigma);
+imshow(hybrid_im)
